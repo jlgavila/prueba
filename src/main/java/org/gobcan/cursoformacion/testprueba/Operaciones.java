1b14ff54dd23a82a82196030ca7c814b65465f12
jlgavila <84497327+jlgavila@users.noreply.github.com>
2021-05-21 10:49:50 +0100

    nuestro primer commit

---
+++ src/main/java/org/gobcan/cursoformacion/testprueba/Operaciones.java
@@ -0,0 +1,22 @@
+package org.gobcan.cursoformacion.testprueba;
+
+/**
+ * Hello world!
+ *
+ */
+public class Operaciones 
+{
+	
+	public static int resta(int a, int b) {
+	    return a-b;
+	}
+	
+	public static int multiplicacion(int a, int b) {
+	    return a*b;
+	}
+	
+	public static int division(int a, int b) {
+		return a/b;
+	}	
+	
+}
